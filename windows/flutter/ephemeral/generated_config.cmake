9db5e9059016c2042a205d984a09951e2b880334
Yash Tailor <61384947+tayloryash@users.noreply.github.com>
2022-08-02 22:12:41 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutterDev\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Yash\\Desktop\\MyShop\\myshop" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutterDev\\flutter"
+  "PROJECT_DIR=C:\\Users\\Yash\\Desktop\\MyShop\\myshop"
+  "FLUTTER_ROOT=C:\\flutterDev\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Yash\\Desktop\\MyShop\\myshop\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Yash\\Desktop\\MyShop\\myshop"
+  "FLUTTER_TARGET=C:\\Users\\Yash\\Desktop\\MyShop\\myshop\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Yash\\Desktop\\MyShop\\myshop\\.dart_tool\\package_config.json"
+)
